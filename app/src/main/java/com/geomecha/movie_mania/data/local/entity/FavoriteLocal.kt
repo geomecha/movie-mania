8804bea4932ba2ce1962a5bbadd2804dd8b7d94f
Viktoriia Bilash <viktoriyabilash@gmail.com>
2024-08-12 09:02:25 +0300

    [ANDROID] Added column names.

--- app/src/main/java/com/geomecha/movie_mania/data/local/entity/FavoriteLocal.kt
+++ app/src/main/java/com/geomecha/movie_mania/data/local/entity/FavoriteLocal.kt
@@ -1,11 +1,14 @@
 package com.geomecha.movie_mania.data.local.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "favorites")
 class FavoriteLocal(
     @PrimaryKey
+    @ColumnInfo(name = "videoId")
     val videoId: Int,
+    @ColumnInfo(name = "isFavorite")
     val isFavorite: Boolean
 )
